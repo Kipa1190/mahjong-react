ee4eea81ee8422721cadc5c472212f9af8da24dc
Lucas Taylor <phantomlimb21@gmail.com>
2021-12-03 20:55:26 -0800

    Testy

---
+++ src/components/BodyComponent.js
@@ -0,0 +1,26 @@
+import React, { Component }  from 'react';
+import { Navbar, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom'; 
+
+class Body extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="container">
+                        <div className="row">
+                            <div className="col">
+
+                            </div>
+                        </div>
+                </div>
+            </React.Fragment>
+            );
+    }
+}
+
+export default Body;
